ee0aa9b32fb62bab5083d45d734a5204cd91ae29
Spythere <spythere@protonmail.com>
2022-07-23 23:06:28 +0200

    Stopka z wersją

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,27 @@
 <template>
   <div id="app_wrapper">
     <router-view />
+
+    <footer>
+      &copy; <a href="https://td2.info.pl/profile/?u=20777">Spythere</a> {{ new Date().getUTCFullYear() }} | v.{{
+        appVersion
+      }}
+    </footer>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import packageInfo from '../package.json';
+
+export default defineComponent({
+  data() {
+    return {
+      appVersion: packageInfo.version,
+    };
+  },
+});
+</script>
 
 <style lang="scss">
 @import './styles/global.scss';
@@ -11,6 +30,11 @@
   color: white;
 
   min-height: 100vh;
+}
+
+footer {
+  text-align: center;
+  padding: 0.5em 0;
 }
 
 @media screen and (max-width: 500px) {
