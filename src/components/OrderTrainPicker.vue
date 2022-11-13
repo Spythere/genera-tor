47edfca3cb7c128e2fadd4f4b08ba0823503b3ba
Spythere <spythere@protonmail.com>
2022-11-13 23:01:07 +0100

    Train picker (wip)

---
+++ src/components/OrderTrainPicker.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="order-train-picker"></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'order-train-picker',
+  data() {
+    return {
+      sceneriesOnline: []
+    }
+  },
+
+  mounted() {
+   
+    
+  },
+
+  methods: {
+    fetchSceneriesOnline() {
+
+    }
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
+
---
+++ src/types/apiTypes.ts
@@ -0,0 +1,7 @@
+export interface IStationsOnlineAPI {
+
+}
+
+export interface IStationsOnlineAPIList {
+
+}
