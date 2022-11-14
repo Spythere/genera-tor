3176ff13b274004635fdfc776a9e05595269e53c
Spythere <spythere@protonmail.com>
2022-11-15 00:23:17 +0100

    Test SW

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,18 @@ import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
 
+import { registerSW } from 'virtual:pwa-register';
+
+const intervalMS = 1 * 60 * 1000;
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    console.log('Need refresh');
+  },
+  onOfflineReady() {
+    console.log('Offline ready');
+  },
+});
 
 createApp(App).use(router).use(createPinia()).mount('#app');
+
