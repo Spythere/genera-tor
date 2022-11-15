956d29f5bca5200a2ea37ce947bd064d4bce521c
Spythere <spythere@protonmail.com>
2022-11-15 17:56:27 +0100

    Prompt przy aktualizacji aplikacji

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,5 @@ import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
 
-import { registerSW } from 'virtual:pwa-register';
-
-const intervalMS = 1 * 60 * 1000;
-
-const updateSW = registerSW({
-  onNeedRefresh() {
-    console.log('Refreshing page...');
-  },
-  
-  onOfflineReady() {
-    console.log('Offline ready');
-  },
-});
-
 createApp(App).use(router).use(createPinia()).mount('#app');
 
